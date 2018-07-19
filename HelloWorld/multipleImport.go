93f903c336714bf8de531d9da15a7e7f9e3fe170
techemayo <muhammadasifmayo@gmail.com>
2018-07-19 20:51:01 +0500

    random number generation functionality implemented

--- HelloWorld/multipleImport.go
+++ HelloWorld/multipleImport.go
@@ -1,6 +1,6 @@
 package main
 import ("fmt"
-       "math")
+       "math/rand")
 
 
 //func foo(){
@@ -10,4 +10,5 @@ import ("fmt"
 func main(){
 //    foo()
 //    fmt.Println("\nThe square root of 64 is : ",math.Sqrt(64))
+	fmt.Println("\n A random number between 1 and 100 : ", rand.Intn(100))
 }
