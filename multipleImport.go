b03c84e6ef5125ee6d3b57fcf3bf1661b48c5c93
techemayo <muhammadasifmayo@gmail.com>
2018-07-19 20:35:27 +0500

    miltiple library import and module from library import

---
+++ multipleImport.go
@@ -0,0 +1,13 @@
+package main
+import ("fmt"
+       "math")
+
+
+func foo(){
+    fmt.Println("\nThe square root of 64 is : ",math.Sqrt(64))
+}
+
+func main(){
+    foo()
+//    fmt.Println("\nThe square root of 64 is : ",math.Sqrt(64))
+}
