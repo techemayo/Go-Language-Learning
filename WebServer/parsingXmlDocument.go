09dea7dfced5a496c5fcc0613b1d5e0b8953df87
techemayo <muhammadasifmayo@gmail.com>
2018-07-20 13:34:31 +0500

    some list formating for location

--- WebServer/parsingXmlDocument.go
+++ WebServer/parsingXmlDocument.go
@@ -13,12 +13,16 @@ type Location struct{
 	Loc string `xml:"loc"`
 }
 
+func (l Location) String()  string {
+	return fmt.Sprintf(l.Loc)
+}
+
 // [5] type == array //e.g: [5] int
 // [] type == slice //e.g: [] int 
 
 
 func main(){
-	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml") //get request 
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml") //get request _ is used for the variable that we donot intend to use otherwise go will throw an error
 	bytes, _ := ioutil.ReadAll(resp.Body) //reading byte from body of response byte data
 	stringBody := string(bytes) //typecasting byte data into string
 	fmt.Println(stringBody) //print string body
