91b7c676993c88bcec96dcd7127cfa8324884bd6
techemayo <muhammadasifmayo@gmail.com>
2018-07-20 15:14:52 +0500

    infinite for loop syntax

--- HelloWorld/loops.go
+++ HelloWorld/loops.go
@@ -6,9 +6,15 @@ func main(){
 	// for i:=0; i<10; i++{
 	// 	fmt.Println(i)
 	// }
-	i:=0
-	for i<10{
-		fmt.Println(i)
-		i++
+	// i:=0
+	// for i<10{
+	// 	fmt.Println(i)
+	// 	i++
+	// }
+
+	// infintie loop
+
+	for{
+		fmt.Println("Do stuff")
 	}
 }
