f987e9ad2773e6e11344d6221e9f5c749b8584b1
techemayo <muhammadasifmayo@gmail.com>
2018-07-20 16:19:06 +0500

    news page details were exracted from site map

--- WebServer/parsingXmlDocument.go
+++ WebServer/parsingXmlDocument.go
@@ -6,32 +6,51 @@ import ("fmt"
 		"encoding/xml")
 
 type SitemapIndex struct {
-	Locations []Location `xml:"sitemap"`
+	Locations []Location `xml:"sitemap>loc"`
 }
 
-type Location struct{
-	Loc string `xml:"loc"`
+type News struct{
+	Titles []string `xml:"url>news>title"`
+	Keywords []string `xml:"url>news>Keywords"`
+	Location []string `xml:"url>loc"`
 }
+// type SitemapIndex struct {
+// 	Locations []Location `xml:"sitemap"`
+// }
 
-func (l Location) String()  string {
-	return fmt.Sprintf(l.Loc)
-}
+// type Location struct{
+// 	Loc string `xml:"loc"`
+// }
+
+// func (l Location) String()  string {
+// 	return fmt.Sprintf(l.Loc)
+// }
 
 // [5] type == array //e.g: [5] int
 // [] type == slice //e.g: [] int 
 
 
 func main(){
-	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml") //get request _ is used for the variable that we donot intend to use otherwise go will throw an error
-	bytes, _ := ioutil.ReadAll(resp.Body) //reading byte from body of response byte data
-	stringBody := string(bytes) //typecasting byte data into string
-	fmt.Println(stringBody) //print string body
-	resp.Body.Close() 		// Close body of byte data
+	// resp, _ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml") //get request _ is used for the variable that we donot intend to use otherwise go will throw an error
+	// bytes, _ := ioutil.ReadAll(resp.Body) //reading byte from body of response byte data
+	// stringBody := string(bytes) //typecasting byte data into string
+	// fmt.Println(stringBody) //print string body
+	// resp.Body.Close() 		// Close body of byte data
 
-	var s SitemapIndex //s variable declared
-	xml.Unmarshal(bytes, &s) //
-	//fmt.Println(s.Locations)
-	for _,Location := range s.Locations{
-		fmt.Printf("\n%s", Location)
+	// var s SitemapIndex //s variable declared
+	byte,_ := iotutil.ReadAll(resp.Body)
+	xml.Unmarshal(bytes,&s)
+	// xml.Unmarshal(bytes, &s) //
+	// //fmt.Println(s.Locations)
+	// for _,Location := range s.Locations{
+	// 	fmt.Printf("\n%s", Location)
+	// }
+
+	var s SitemapIndex
+	var n News
+	resp,_ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
+	for _,Location :=range s.Location{
+		resp,_:=ioutil.ReadAll(resp.Body)
+		xml.Unmarshal(bytes,&n)
 	}
 }
