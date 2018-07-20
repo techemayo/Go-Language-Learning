88cab5dd53bc1400347b714fcdf762d7e8285790
techemayo <muhammadasifmayo@gmail.com>
2018-07-20 09:28:00 +0500

    pointer function implemented

--- HelloWorld/struct.go
+++ HelloWorld/struct.go
@@ -3,6 +3,7 @@ package main // main package is being used
 import "fmt"
 //fmt library imported
 
+
 const usinxteenbitmax float64 = 65535	
 const kmh_multiple float64 = 1.60934 //1 mile is equals to 1.60934 miles
 
@@ -17,14 +18,32 @@ type car struct { //structure car an structure is a composite datatype that is u
 	top_speed_kmh float64
 }
 
+func (c *car) new_top_speed(newspeed float64){ //pointer function here it is used to set new speed in car top speed
+	c.top_speed_kmh = newspeed 					//*car pointer makes it pointer function	
+}												//permanently updates top speed for the structure
+
 func (c car) kmh() float64{  //value receiver function/method
 	// (c car) associate car type to this function and c is used to refrence car struc 
 	 //this function calculate the top speed in kilometer per hour
+	//c.top_speed_kmh = 500 //temporarily set top speed to 500 and do not store in structure
 	return float64(c.gas_pedal)*(c.top_speed_kmh / usinxteenbitmax)
 }
+// func (c *car) kmh() float64{  //pointer function
+// 	c.top_speed_kmh = 500 //permanently set top speed to 500 and do not store in structure
+// 	return float64(c.gas_pedal)*(c.top_speed_kmh / usinxteenbitmax)
+// }
 
-func (c car) mph() float64 { //value reciever function
+func (c *car) mph() float64 { //value reciever function
+	c.top_speed_kmh=500 //permanantly ipdate value of top speed for the car
 	return float64(c.gas_pedal)*(c.top_speed_kmh/usinxteenbitmax/kmh_multiple)
+}
+
+//we use pointer function where we need performace gains
+//value receiver functions are expensive
+
+func newer_car_speed(c car ,speed float64) car{
+	c.top_speed_kmh=speed
+	return c
 }
 
 func main() {
@@ -39,5 +58,10 @@ func main() {
 	//fmt.Println(a_car.gas_pedal)
 
 	fmt.Println( a_car.kmh() )
-	fmt.Println( a_car.mph())
+	fmt.Println( a_car.mph() )
+	// a_car.new_top_speed(500)
+	a_car = newer_car_speed(a_car, 1100)
+	fmt.Println( a_car.kmh() )
+	fmt.Println( a_car.mph() )
+
 }
