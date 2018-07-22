332d95c69b10423691ef709ff6ed05e1caa338be
techemayo <muhammadasifmayo@gmail.com>
2018-07-23 00:23:04 +0500

    go channels synchronization concurrency was used to boost the speed of website

--- HelloWorld/gochannels.go
+++ HelloWorld/gochannels.go
@@ -1,18 +1,35 @@
 package main
 
-import "fmt"
+import ("fmt"
+		"sync")
+
+var wg sync.WaitGroup
 
 func foo(c chan int, someValue int){
+	defer wg.Done()
 	c <- someValue*5
 }
 
 func main(){
-	someVal := make(chan int)
-	go foo (someVal,5)
-	go foo (someVal,3)
+	someVal := make(chan int,10)
+	for i := 0 ; i <10 ; i++{
+		wg.Add(1)
+		go foo(someVal,i)
+	}
+
+	wg.Wait()
+	close(someVal)
+
+	for item := range someVal{
+		fmt.Println(item)
+	}
+
+	// go foo (someVal,5)
+	// go foo (someVal,3)
+
 
 //	v1 := <- someVal
 //	v2 := <- someVal
-	v1,v2 := <- someVal, <- someVal
-	fmt.Println(v1 , v2)
+	// v1,v2 := <- someVal, <- someVal
+	// fmt.Println(v1 , v2)
 }
--- WashingtonPost Aggregator Webpage/aggregator.go
+++ WashingtonPost Aggregator Webpage/aggregator.go
@@ -4,7 +4,10 @@ import ("fmt"
 		"net/http"
 		"html/template"
 		"io/ioutil"
-		"encoding/xml")
+		"encoding/xml"
+		"sync")
+
+var wg sync.WaitGroup
 
 type NewsAggPage struct {
 	Title string
@@ -25,23 +28,35 @@ type NewsMap struct {
 	Location string
 }
 
+func newsRoutine(c chan News,Location string){
+		defer wg.Done()
+		var n News
+		resp,_ := http.Get(Location)
+		bytes,_:=ioutil.ReadAll(resp.Body)
+		xml.Unmarshal(bytes,&n)
+		resp.Body.Close()
+		c <- n
+}
+
 func newsAggHandler(w http.ResponseWriter, r *http.Request){
 	var s SitemapIndex
-	var n News
-
 	
 	resp,_ := http.Get("https://www.washingtonpost.com/news-sitemap-index.xml")
 
 	bytes,_ := ioutil.ReadAll(resp.Body)
 	xml.Unmarshal(bytes,&s)
 	news_map := make(map[string] NewsMap)
+	resp.Body.Close()
+	queue := make(chan News,30)
 	for _,Location :=range s.Locations{
-
-		resp,_ := http.Get(Location)
-		bytes,_:=ioutil.ReadAll(resp.Body)
-		xml.Unmarshal(bytes,&n)
-		for idx,_ :=range n.Titles{
-			news_map[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
+		wg.Add(1)
+		go newsRoutine(queue, Location)
+		}
+		wg.Wait()
+		close(queue)
+		for elem := range queue{
+		for idx,_ :=range elem.Titles{
+		news_map[elem.Titles[idx]] = NewsMap{elem.Keywords[idx], elem.Locations[idx]}
 		}
 	}
 	p:=NewsAggPage{Title: "The Amazing News Aggregator", News: news_map}
