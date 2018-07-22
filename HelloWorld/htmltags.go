afb17582427406d395c1c6f1f4366e927e97b1ee
techemayo <muhammadasifmayo@gmail.com>
2018-07-22 20:12:42 +0500

    Basic html template created and manipulate with html/template library

--- HelloWorld/htmltags.go
+++ HelloWorld/htmltags.go
@@ -1,14 +1,27 @@
 package main
 
 import ("fmt"
-		"net/http")
+		"net/http"
+		"html/template")
+
+type NewsAggPage struct {
+	Title string
+	News string
+}
+
+func newsAggHandler(w http.ResponseWriter, r *http.Request){
+	p:=NewsAggPage{Title: "The Amazing News", News: "Some News"}
+	t,_ := template.ParseFiles("basichtmltemplate.html")
+	fmt.Println(t.Execute(w,p))
+}
 
 func indexHandler(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "<h1>Whoa, Go is Neat!</h1>")
 
 }
 
-func func main() {
+func main() {
 	http.HandleFunc("/",indexHandler)
+	http.HandleFunc("/agg/", newsAggHandler)
 	http.ListenAndServe(":8000",nil)
 }
