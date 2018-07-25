ca1c2763ee643d2af0ebff71ebe5a369116acbe2
techemayo <muhammadasifmayo@gmail.com>
2018-07-25 23:26:39 +0500

    some addition in api's post functionality

--- API/couchapi.go
+++ API/couchapi.go
@@ -17,8 +17,24 @@ func getapifunc(w http.ResponseWriter, r *http.Request) {
 	resp.Body.Close()
 	fmt.Fprintf(w, string(body))
 }
+type posting struct {
+	string "Name" 
+}
+	myname posting = {"asif"}
+func postapifunc(w http.ResponseWriter, r *http.Request) {
+	//resp, err := http.Get("http://127.0.0.1:5984/_all_dbs")
+	//resp, err := http.Post("http://127.0.0.1:5984/baseball2/",url.Values{"key":{"Value"},"name":{"asif"}})
+	resp, err := http.Post("http://127.0.0.1:5984/baseball2/",posting,myname)
+	if err != nil {
+		log.Print(err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	fmt.Fprintf(w, string(body))
+}
 
 func main() {
 	http.HandleFunc("/getapi", getapifunc)
+	http.HandleFunc("/postapi", postapifunc)
 	http.ListenAndServe(":8000", nil)
 }
