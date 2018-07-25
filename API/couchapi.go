8fd2e889d313fd2088eef6878f572f486a806a1c
techemayo <muhammadasifmayo@gmail.com>
2018-07-25 17:36:48 +0500

    go and couch db get method implemented

--- API/couchapi.go
+++ API/couchapi.go
@@ -3,19 +3,22 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 )
 
 func getapifunc(w http.ResponseWriter, r *http.Request) {
-	resp, err := http.Get("http://127.0.0.1:5984/_all_dbs")
+	//resp, err := http.Get("http://127.0.0.1:5984/_all_dbs")
+	resp, err := http.Get("http://127.0.0.1:5984/baseball2/8b8b6e539c5f806fe5e8aa50a00033f6")
 	if err != nil {
-		fmt.Fprintf(w, err)
+		log.Print(err)
 	}
 	body, err := ioutil.ReadAll(resp.Body)
 	resp.Body.Close()
-	fmt.Fprintf(w, body)
+	fmt.Fprintf(w, string(body))
 }
 
 func main() {
 	http.HandleFunc("/getapi", getapifunc)
+	http.ListenAndServe(":8000", nil)
 }
